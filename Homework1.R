fc0332a38a8c434e1be68c1848c8de02ca8dc986
Raf <Rafcruyff@gmail.com>
2023-03-15 17:21:38 +0100

    Finished homework 1, looks reasonable. No answers though.

--- Homework1.R
+++ Homework1.R
@@ -16,10 +16,47 @@ num_events = sum(d)
 # of the uncensored times using log likelihood
 
 # For this we take the product of f_X(t_i|lambda)f_Y(d_i) for all i as likelihood
-# then 100log(lambda/3)-sum_(i=1)^100 t_i  will be log-likelihood.
+# then 100log(lambda/3)-sum_(i=1)^100 x_i  will be log-likelihood.
 # and MLE will be
-MLE_u = n/sum(tt)
+MLE_u = n/sum(x)
 
 # Compute the maximum likelihood estimator
 # of the uncensored times using log likelihood
 MLE_c = num_events/sum(tt)
+
+# Make a plot of the log-likelihood. first make a fine grid on (0.1,0.3)
+grid = 0.1 + (1:200)/1000
+# compute the log-likelihoods
+l = log(grid)*57-grid*sum(tt) 
+# Plot it
+plot(grid, l)
+# Find the maximum log likelihood
+l_max = max(l)
+lambda_est = grid[which.max(l)]
+# Find lambda_up and lambda_down.
+l_deviate = l_max-1.92 
+lambda_down = grid[min(which(l > l_deviate))]
+lambda_up = grid[max(which(l > l_deviate))]
+# Plot in the graph
+abline(v = lambda_down, col = "darkgreen")
+abline(v = lambda_up, col = "darkgreen")
+abline(h = l_deviate, col = "red")
+
+# Now we do the same for the uncensored data.
+
+# compute the log-likelihoods for the uncensored data
+l_u = n*log(grid/3)-grid*sum(x) 
+# Plot it
+plot(grid, l_u)
+# Find the maximum log likelihood
+l_u_max = max(l_u)
+lambda_est = grid[which.max(l_u)]
+# Find lambda_up and lambda_down.
+l_u_deviate = l_u_max-1.92 
+lambda_u_down = grid[min(which(l_u > l_u_deviate))]
+lambda_u_up = grid[max(which(l_u > l_u_deviate))]
+# Plot in the graph
+abline(v = lambda_u_down, col = "darkgreen")
+abline(v = lambda_u_up, col = "darkgreen")
+abline(h = l_u_deviate, col = "red")
+
