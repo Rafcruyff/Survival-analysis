680b64936b656c704f32eb104781ad6bc02cec43
Rafcruyff <rafcruyff@gmail.com>
2023-03-16 13:54:06 +0100

    tried to implement NA estimator. Pretty difficult still

--- Homework3.R
+++ Homework3.R
@@ -1,5 +1,23 @@
+# Loading discfrail and survival
+library(discfrail)
+library(survival)
+function(nelsonaalen_npdf)
+
 # Loading the leukemia data 
 library(KMsurv)
-data <- data("drug6mp")
+data("drug6mp")
 # Data info
 ?drug6mp
+# Compute the Nelson-Aalen estimator for both groups
+# First we define a timeline
+t = 1:350/10
+
+# Placebo:
+t_p = drug6mp['t1']
+# Compute number at risk
+y_p = rep(21,350)
+for (i in t_p){
+  y_p[which(y_p>i)] = y_p[which(y_p>i)]-1
+}
+  
+NA_p = nelsonaalen_npdf(t_p)
