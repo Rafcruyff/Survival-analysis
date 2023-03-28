7cd85f8e4899342b9d4eab3d2c0d6af6ab31a973
Rafcruyff <rafcruyff@gmail.com>
2023-03-28 11:04:47 +0200

    Almost got the first question with the CI. I think I may be getting the variance wrong. I don't know

--- Homework4.R
+++ Homework4.R
@@ -10,11 +10,31 @@ d <- drug6mp
 
 # The Kaplan Meier estimator without using the survival package.
 
-d <- arrange(d, t1)
-for (i in 1:21) {
-  d$at_risk[i] <- sum(d$t1 >= d$t1[i])
+t <- 1:max(d$t1)
+dfKM <- data.frame(t=t) %>%
+  mutate(at_risk = 0,
+         events = 0,
+         censor = 0)
+
+for (i in t){
+  dfKM$at_risk[i] <- sum(d$t1 >= i)
+  dfKM$events[i] <- nrow(d[d$t1 == i & d$relapse == 1,])
+  dfKM$censor[i] <- nrow(d[d$t1 == i & d$relapse == 0,])
 }
-d <- mutate(d, KM = cumprod(1-1/at_risk))
+
+z <- qnorm(.975)
+
+dfKM <- mutate(dfKM,
+               KM = cumprod(1-events/at_risk),
+               KM_var = sqrt(KM^2*cumsum(events/(at_risk*(at_risk-events)))),
+               KM_min = KM^(exp(KM_var/(KM*log(KM)))),
+               KM_plus = KM^(exp(-KM_var/(KM*log(KM)))))
+
+
+
+plot(dfKM$KM, type = 's')
+lines(t, dfKM$KM_min, type = "s", lty = 2)
+lines(t, dfKM$KM_plus, type = "s", lty = 2)
 
 # The Kaplan Meier estimator using the survival package.
 
