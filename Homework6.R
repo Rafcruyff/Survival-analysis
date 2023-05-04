0c88fc0a1a8b50e06b76d20dc294b16b1449c3be
Raf <Rafcruyff@gmail.com>
2023-05-04 16:58:35 +0200

    Did some work on HW^ with my friend GPT. Bit too tired to really understand what is going on. Better luck tomorrow.

--- Homework6.R
+++ Homework6.R
@@ -1,3 +1,19 @@
+# Load the survival library
+library(survival)
+
 # Reading and preprocessing the data
 data = read.table("/home/raf/Survival\ analysis/Oscar.txt", header = TRUE, sep = "\t")
 data['Age'] <- data['Final']-data['Birth']
+
+# The survival object
+surv_obj <- Surv(data$Age, data$Alive)
+# Plotting the overall survival curve
+km_fit <- survfit(surv_obj ~ 1)
+mean_surv_time <- integrate(function(x) predict(km_fit, x, type = "surv"), 0, max(data$Final))$value
+
+plot(km_fit, xlab = "Time (years)", ylab = "Survival Probability", main = "Kaplan-Meier Survival Curve")
+
+
+# The log-rank test
+group <- factor(data$Wins)
+survdiff(surv_obj ~ group)
